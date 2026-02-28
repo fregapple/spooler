9b46dad0114203af54762ab3549efbeba0a3647d
fregapple <altstore4k@gmail.com>
2026-02-28 13:37:55 +1100

    .

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/fregapple/spooler.git /app/repo
-cp /app/repo/spooler /app/spooler
+cp -r /app/repo/spooler /app/spooler
 cp /app/repo/config/config_example.json /app/config/config_example.json
 cd /app/spooler
 ./run.sh
