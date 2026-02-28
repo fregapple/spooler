e8f5ea310ab37e6304dac6ecb220ab478164a263
fregapple <altstore4k@gmail.com>
2026-02-28 13:31:33 +1100

    Changing the way the docker mount would work to effectively read the file.

--- spooler/bootstrap.sh
+++ spooler/bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 git clone https://github.com/fregapple/spooler.git /app/repo
-cd /app/repo
+cp /app/repo/spooler/spooler /app/spooler
+cp /app/repo/spooler/config/config_example.json /app/config/config_example.json
+cd /app/spooler
 ./run.sh
