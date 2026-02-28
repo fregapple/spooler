269c4e696974de8f776959d7f3d69ee2fdbd1b77
fregapple <altstore4k@gmail.com>
2026-02-28 13:46:26 +1100

    Changed bootstrap to only clone 1 time. Not every container restart.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-git clone https://github.com/fregapple/spooler.git /app/repo
-cp -r /app/repo/spooler /app/spooler
-cp /app/repo/config/config_example.json /app/config/config_example.json
+
+if [ ! -d "/app/spooler" ]; then
+    echo "[BOOTSTRAP] First run: Cloning repo ..."
+    git clone https://github.com/fregapple/spooler.git /app/repo
+    
+    echo "[BOOTSTRAP] Copying code into /app/spooler ..."   
+    cp -r /app/repo/spooler /app/spooler
+    cp /app/repo/config/config_example.json /app/config/config_example.json
+
+else
+    echo "[BOOTSRAP] Code already present, skipping clone and copy."
+fi
+
 cd /app/spooler
 ./run.sh
